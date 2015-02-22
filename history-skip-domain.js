a830ea7d1bf3c1378f0b298b9baf610bf084ff2d
Denis Kasak <denis.kasak@gmail.com>
2015-02-22 15:31:22 +0100

    history-skip-domain: Add licence and documentation.

--- history-skip-domain.js
+++ history-skip-domain.js
@@ -1,3 +1,62 @@
+/*
+ * history-skip-domain.js
+ *
+ * Copyright (C) 2015 Denis Kasak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+/* Adds mappings to go back or forward in history to a previous/next domain (as
+ * in Pentadactyl).
+ *
+ * Mappings:
+ *   <count>[d - go back to a <count>th previous domain in history
+ *   <count>]d - go forward to a <count>th next domain in history
+ */
+
+var INFO = xml`
+<plugin name="history-skip-domain" version="1.0"
+        href="https://github.com/dkasak/vimperator-plugins/blob/master/history-skip-domain.js"
+        summary="Skip to a previous/next domain in browser history"
+        lang="en_US"
+        xmlns="http://vimperator.org/namespaces/liberator">
+    <author email="reversed(ku.gro.animret@kasakd)">Denis Kasak</author>
+    <project name="Vimperator" minVersion="3.6"/>
+    <license>MIT</license>
+    <p>Adds mappings to go back or forward in history to a previous/next domain.</p>
+    <item>
+        <tags>[d</tags>
+        <spec>[count][d</spec>
+        <description>
+            <p>Go back to a [count]th previous domain in history.</p>
+        </description>
+    </item>
+    <item>
+        <tags>]d</tags>
+        <spec>[count]]d</spec>
+        <description>
+            <p>Go forward to a [count]th next domain in history.</p>
+        </description>
+    </item>
+</plugin>`;
+
 let history = liberator.modules.history;
 let mappings = liberator.modules.mappings;
 
